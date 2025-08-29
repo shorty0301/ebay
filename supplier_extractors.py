bd3375fe457e96fac5a6138c0cfc15532322a22d
shorty0301 <haatanshorty.azm@gmail.com>
2025-08-29 16:35:31 +0900

    Update supplier_extractors.py

--- supplier_extractors.py
+++ supplier_extractors.py
@@ -884,7 +884,7 @@ def extract_supplier_info(url: str, html: str, debug: bool = False) -> Dict[str,
         s = stock_from_surugaya(html, text)
         if s: stock = s
         price = price_from_surugaya(html, text)
-　　    # --- Amazon.co.jp ---
+　　# --- Amazon.co.jp ---
     elif ("amazon.co.jp" in host) or ("www.amazon.co.jp" in host):
         s = stock_from_amazon_jp(html, text)
         if s: stock = s
